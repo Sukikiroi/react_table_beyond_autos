edf5a149b20b55f165ee19200ce89c8488ddda03
Belkis Asma SEMCHEDDINE <semcheddineasma@yahoo.fr>
2021-11-16 18:08:24 +0100

    git bug fixed

--- makeData.js
+++ makeData.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import namor from 'namor'
 
 const range = len => {
@@ -44,50 +43,3 @@ export default function makeData(...lens) {
 
   return makeDataLevel()
 }
-=======
-import namor from 'namor'
-
-const range = len => {
-  const arr = []
-  for (let i = 0; i < len; i++) {
-    arr.push(i)
-  }
-  return arr
-}
-
-const newPerson = () => {
-  const statusChance = Math.random()
-  return {
-    firstName: namor.generate({ words: 1, numbers: 0 }),
-    password: namor.generate({ words: 1, numbers: 0 }),
-    car:{
-      imgsrc:'imagesource',
-      name:'',
-    },
-    lastName: namor.generate({ words: 1, numbers: 0 }),
-    age: Math.floor(Math.random() * 30),
-    visits: Math.floor(Math.random() * 100),
-    progress: Math.floor(Math.random() * 100),
-    status:
-      statusChance > 0.66
-        ? 'relationship'
-        : statusChance > 0.33
-        ? 'complicated'
-        : 'single',
-  }
-}
-
-export default function makeData(...lens) {
-  const makeDataLevel = (depth = 0) => {
-    const len = lens[depth]
-    return range(len).map(d => {
-      return {
-        ...newPerson(),
-        subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
-      }
-    })
-  }
-
-  return makeDataLevel()
-}
->>>>>>> a38dffc713f4fd1a2792444ec7051b9bd6498507
